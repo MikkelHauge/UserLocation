c7c4a3f2e5ef23843a8d312e2572dda1604b4a9e
Mikkel Hauge <mikkelhauge84+github@gmail.com>
2023-03-10 07:03:25 +0100

    Initial Commit

---
+++ UserLocation/UserLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserLocationApp.swift
+//  UserLocation
+//
+//  Created by Mikkel Hauge on 10/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct UserLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
